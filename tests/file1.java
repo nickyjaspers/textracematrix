6447b3a21aae2540ebfa218de9cab5451b41b089
Nicky Jaspers <nicky_jaspers@hotmail.com>
2014-11-04 20:25:50 +0100

    initial commit

---
+++ tests/file1.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package projectone;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author nicky
+ *
+ */
+public class TestA {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    
+    // REQ: as
+    @Test   
+    public void testOne(){
+        fail("Not yet implemented");    
+    }
+    
+}
